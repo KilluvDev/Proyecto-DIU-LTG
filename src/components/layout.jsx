f1080a783f198495c585be40cc3d0e0bf0c5209e
Ignacio Trujillo <83096192+KilluvDev@users.noreply.github.com>
2024-11-02 19:51:34 -0300

    Update layout.jsx

--- src/components/layout.jsx
+++ src/components/layout.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 import HomePage from '../pages/home_page';
+import PersonalPage from '../pages/personal_page';
 import NavBar from '../components/nav_bar';
 
 const Layout = () => {
@@ -13,6 +14,7 @@ const Layout = () => {
         <div className='layout__page'>
           <Routes>
             <Route path='/' element={<HomePage />} />
+            <Route path='/personal-info' element={<PersonalPage />} />
             {/* Agrega más rutas aquí si tienes otras páginas */}
           </Routes>
         </div>
